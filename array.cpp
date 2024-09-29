eaba2c918f68e67018f90381928829add9979b9e
Rahul Kumar Ravi <rkr.rahulravi@gmail.com>
2024-09-29 16:04:00 +0530

    learn array today

---
+++ array.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+
+{
+	int A[5];
+
+	int i;
+
+	for(i=0;i<5;i++)
+	{
+		cin >> A[i];
+	}
+
+	for(i=0;i<5;i++)
+	{
+		cout << A[i] << " " ;
+	}
+
+ 
+
+	return 0;
+}
